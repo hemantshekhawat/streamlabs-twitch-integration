082b59fdc6313ab5d8c681a814927f160aa929ba
Hemant Singh <hemant.singh@payu.in>
2019-03-04 20:50:50 +0530

    laravel migration bug fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',254);
+            $table->string('email',254)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',32);
             $table->rememberToken();
             $table->timestamps();
         });
